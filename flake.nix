7ee2a7e302b181f9f85d82211133fbe274e28fd2
Tom <tomboos@gmx.de>
2025-08-13 18:17:51 +0200

    adds prism flake

--- flake.nix
+++ flake.nix
@@ -8,11 +8,20 @@
       url = "github:ezKEa/aagl-gtk-on-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    prismlauncher = {
+      url = "github:PrismLauncher/PrismLauncher";
+
+      # Optional: Override the nixpkgs input of prismlauncher to use the same revision as the rest of your flake
+      # Note that this may break the reproducibility mentioned above, and you might not be able to access the binary cache
+      #
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    prismlauncher,
     ...
   } @ inputs: let
     system = "x86_64-linux";
@@ -24,6 +33,11 @@
         ./configuration.nix
         ./hardware-configuration.nix
         {nixpkgs.hostPlatform = system;}
+        {
+          environment.systemPackages = [
+            inputs.prismlauncher.packages.${system}.prismlauncher
+          ];
+        }
       ];
     };
   };
