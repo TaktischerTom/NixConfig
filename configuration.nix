8b6542e3e25538ece0043e5808f6e9107b725b47
Tom <tomboos@gmx.de>
2025-08-13 23:33:05 +0200

    jvm args

--- configuration.nix
+++ configuration.nix
@@ -161,7 +161,9 @@
     servers = {
       fabricLatest = {
         enable = true;
+        autoStart = false;
         package = pkgs.fabricServers.fabric;
+        jvmOpts = "-Xms8192M -Xmx8192M";
       };
     };
   };
