fe72a9209370516a0b596ac71c1d7e93668f91ae
Tom Boos <tomboos@gmx.de>
2026-06-20 02:55:22 +0200

    anable ntsync

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -3,4 +3,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelModules = [ "ntsync" ];
+  services.udev.extraRules = ''
+    KERNEL=="ntsync", MODE="0666"
+  '';
 }
