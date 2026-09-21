7cd8a264120f6e9a65af5f6b6168460aa5de8499
Tom Boos <tomboos@gmx.de>
2026-09-21 05:51:39 +0200

    added some nix-ld config

--- modules/programs/common.nix
+++ modules/programs/common.nix
@@ -1,7 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.firefox.enable = true;
   programs.thunar.enable = true;
   programs.yazi.enable = true;
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      fontconfig
+      freetype
+      stdenv.cc.cc.lib
+      
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXrandr
+      xorg.libXrender
+      
+      wayland
+      libdecor
+      libxkbcommon
+      
+      alsa-lib
+      vulkan-loader
+      libGL
+    ];
+  };
 }
